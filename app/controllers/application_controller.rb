8ffd15cf5fbf6aa32ea7124a4f9340cf1e45761b
PreetiDantkale <dantkalepreeti29@gmail.com>
2020-01-10 14:26:48 +0530

    first commit (devise gem setup

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+class ApplicationController < ActionController::Base
+
+  before_action :authenticate_user!
+  
+  def after_sign_in_path_for(resource_or_scope)
+    dashboard_index_url
+  end
+
+  def after_sign_up_path_for(resource_or_scope)
+    dashboard_index_url
+  end
+end
