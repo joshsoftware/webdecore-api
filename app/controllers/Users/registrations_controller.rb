fdf69587dc3051ee71dd082198a73c03887d6e0a
SuccessMaheshwari <yash.maheshwari@joshsoftware.com>
2020-01-13 13:10:14 +0530

    Bootstrap applied

--- app/controllers/Users/registrations_controller.rb
+++ app/controllers/Users/registrations_controller.rb
@@ -40,9 +40,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
  # protected
 
  # If you have extra params to permit, append them to the sanitizer.
-  def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  end
+   def configure_sign_up_params
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute, :first_name, :last_name, :contact_number])
+   end
 
  # If you have extra params to permit, append them to the sanitizer.
  # def configure_account_update_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    root 'devise/sessions#new'
+  end
   devise_for :users
   resources :dashboard
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
