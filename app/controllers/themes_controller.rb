8132cdd0293adfe30151a90e7698cd5bef9babeb
PreetiDantkale <dantkalepreeti29@gmail.com>
2020-01-27 10:44:14 +0530

    Modified Themes Controller

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -2,8 +2,6 @@
 
 class ThemesController < ApplicationController
   def index
-    # @themes = Theme.find_by_user_id(current_user.id)
-    # binding.pry
     @themes = Theme.all
   end
 
@@ -12,25 +10,27 @@ class ThemesController < ApplicationController
   end
 
   def create
-    @theme = Theme.new(theme_params)
-    @theme.user_id = current_user.id
-
-    @theme.state = if @theme.state == '1'
-                     'Active'
-                   else
-                     'InActive'
-                   end
-
-    redirect_to @theme if @theme.save!
+    @theme = current_user.themes.new(theme_params)
+    if @theme.save!
+      redirect_to @theme
+    else
+      render 'new'
+    end
   end
 
   def show
-    @theme = Theme.find(params[:id])
+    @theme = Theme.find_by(id: params[:id])
+
+    unless @theme
+      flash[:alert] = t('activerecord.exceptions.record_not_found')
+      redirect_to themes_path
+    end
   end
 
   private
+  
+    def theme_params
+      params.require(:theme).permit(:user_id, :theme_name, :active ,:animation)
+    end
 
-  def theme_params
-    params.require(:theme).permit(:user_id, :theme_name, :state, :animation)
-  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_117_060_355) do
+ActiveRecord::Schema.define(version: 2020_01_27_120624) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -25,31 +25,41 @@ ActiveRecord::Schema.define(version: 20_200_117_060_355) do
     t.index ['theme_id'], name: 'index_assets_on_theme_id'
   end
 
-  create_table 'themes', force: :cascade do |t|
-    t.string 'theme_name'
-    t.string 'state'
-    t.bigint 'user_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'animation'
-    t.index ['user_id'], name: 'index_themes_on_user_id'
+  create_table "roles", force: :cascade do |t|
+    t.string "role_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'first_name'
-    t.string 'last_name'
-    t.string 'contact_number'
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "themes", force: :cascade do |t|
+    t.string "theme_name"
+    t.boolean "active"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "animation"
+    t.index ["user_id"], name: "index_themes_on_user_id"
   end
 
-  add_foreign_key 'assets', 'themes'
-  add_foreign_key 'themes', 'users'
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "contact_number"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "role_id", default: 2, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["role_id"], name: "index_users_on_role_id"
+  end
+
+  add_foreign_key "assets", "themes"
+  add_foreign_key "themes", "users"
+  add_foreign_key "users", "roles"
+
 end
