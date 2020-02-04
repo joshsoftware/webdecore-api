b0272afbe1123bd3440d512afa32423167c6080d
Anurag <anurag.ganjiwale97@gmail.com>
2020-02-04 18:17:57 +0530

    Routes updated and removed comments

--- app/controllers/animation_datas_controller.rb
+++ app/controllers/animation_datas_controller.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
 class AnimationDatasController < ApplicationController
-
   def show
-    @animations = AnimationData.where(category_id: params[:category_id])
+    @animations = AnimationData.where(category_id: params[:sub_category_id])
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,30 @@ Rails.application.routes.draw do
     root 'users/sessions#new'
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
+  
   resources :themes
   resources :dashboard
-  resources :categories do
-    resources :animation_datas
+  resources :categories
+  resources :animation_datas
+
+  scope "/categories/:id/" do
+    get "sub_categories" => 'categories#show'
+
+    scope "sub_categories/:sub_category_id/" do
+      get "animations" => 'animation_datas#show'
+    end
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do  
+    namespace :v1 do
+      resources :animation_datas
+    end
+  end
+
+  namespace :api do  
+    namespace :v2 do
+      resources :animation_datas
+    end
+  end
+
 end
