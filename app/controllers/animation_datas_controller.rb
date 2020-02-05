4c26d5fd1e13d92c7f1a92df0af4d55e2ff132be
PreetiDantkale <dantkalepreeti29@gmail.com>
2020-02-05 11:04:06 +0530

    Created User file after registration

--- app/controllers/animation_datas_controller.rb
+++ app/controllers/animation_datas_controller.rb
@@ -4,5 +4,4 @@ class AnimationDatasController < ApplicationController
   def show
     @animations = AnimationData.where(category_id: params[:sub_category_id])
   end
-  
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 class ApplicationController < ActionController::Base
   include Pundit
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
   def after_sign_in_path_for(_resource_or_scope)
@@ -16,10 +17,16 @@ class ApplicationController < ActionController::Base
     dashboard_index_url
   end
 
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :contact_number])
+    end
+
   private
 
-  def user_not_authorized
-    flash[:alert] = t('user_not_authorized')
-    redirect_to(request.referrer || dashboard_index_url)
-  end
+    def user_not_authorized
+      flash[:alert] = t('user_not_authorized')
+      redirect_to(request.referrer || dashboard_index_url)
+    end
 end
