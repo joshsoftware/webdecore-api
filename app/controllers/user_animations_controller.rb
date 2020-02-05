2276db314a9509d5fca7bbd6f4ea24fc76099d6f
Anurag <anurag.ganjiwale97@gmail.com>
2020-02-05 16:18:14 +0530

    API Done

--- app/controllers/user_animations_controller.rb
+++ app/controllers/user_animations_controller.rb
@@ -1,5 +1,24 @@
 # frozen_string_literal: true
 
 class UserAnimationsController < ApplicationController
-  def index; end
+  def index
+    @animations = UserAnimation.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+
+    # def new
+    #     @farmer_list = Farmer.all.collect {|u| [u.full_name, u.id]}
+    #     @instrument_list = Instrument.all.collect {|u| [u.name, u.id]}
+    # end
+
+    # def create
+    #     @advertise = FarmerInstrument.create(permit_params)
+    #     redirect_to farmer_instruments_path
+    end
 end
