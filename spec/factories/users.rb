9708cec7ce38ffd1126c1a6fbd705307113c4faa
Anurag <anurag.ganjiwale97@gmail.com>
2020-02-10 12:19:26 +0530

    test cases for model added

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    first_name         { Faker::Name.first_name }
+    last_name          { Faker::Name.last_name }
+    contact_number     { Faker::PhoneNumber.phone_number }
+    email              { Faker::Internet.email }
+    password           { Faker::Number.number(digits: 7) }
+  end
+end
---
+++ spec/models/user_animation_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+require 'factories/user_animations'
+
+RSpec.describe UserAnimation, type: :model do
+  before(:each) do
+    @user_animation = create :user_animation
+  end
+  it 'should save valid user animation record' do
+    expect(@user_animation.valid?).to eq true
+    expect(@user_animation.save).to eq true
+  end
+
+  it 'should not save without valid start date' do
+    @user_animation = build(:user_animation, start_date: nil)
+    expect(@user_animation.valid?).to eq false
+  end
+
+  it 'should not save without valid end date' do
+    @user_animation = build(:user_animation, end_date: nil)
+    expect(@user_animation.valid?).to eq false
+  end
+
+  it 'should not save without valid location' do
+    @user_animation = build(:user_animation, location: nil)
+    expect(@user_animation.valid?).to eq false
+  end
+
+  it 'should give error message' do
+    @user_animation.valid?
+    expect(@user_animation.errors.messages.blank?).to eq true
+  end
+
+end
