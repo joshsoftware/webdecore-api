41c9fe51603027a51b48fcc1a16b94b8307d5e12
Anurag <anurag.ganjiwale97@gmail.com>
2020-02-12 12:24:30 +0530

    Validation for date added

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :user do
+    role
     first_name         { Faker::Name.first_name }
     last_name          { Faker::Name.last_name }
     contact_number     { Faker::PhoneNumber.phone_number }
