f859c35d48f3364b732acf904ee65608091525e7
SuccessMaheshwari <yash.maheshwari@joshsoftware.com>
2020-02-13 18:33:23 +0530

    added edit,delete,show order

--- app/controllers/api/v1/animation_datas_controller.rb
+++ app/controllers/api/v1/animation_datas_controller.rb
@@ -3,9 +3,10 @@ module Api
     class AnimationDatasController < ApplicationController
       skip_before_action :authenticate_user!
       def index
-        
-        @animation_record = UserAnimation.find_by("user_id = ? AND location = ? AND start_date <= ? 
-        AND end_date >= ?", params['user_id'], params['location'], Date.today, Date.today)
+
+        @animation_record = UserAnimation.find_by("user_id = ? AND location = ? AND start_date <= ?
+        AND end_date >= ? AND status = ?", params['user_id'], params['location'], Date.today, Date.today, "Active")
+
         if (@animation_record)
           @animation = AnimationData.find(@animation_record.animation_data_id).animation_json
           render json: @animation.as_json
@@ -14,7 +15,6 @@ module Api
         end
 
       end
-
     end
   end
 end
--- app/controllers/user_animations_controller.rb
+++ app/controllers/user_animations_controller.rb
@@ -15,10 +15,9 @@ class UserAnimationsController < ApplicationController
 
   def create
     if validate_date
-      puts permit_params
       user_animation = current_user.user_animations.new(permit_params)
       user_animation.status = "Active"
-      if user_animation.save!
+      if user_animation.save
         redirect_to user_animations_path
       else
         redirect_to_new
@@ -28,6 +27,42 @@ class UserAnimationsController < ApplicationController
     end
   end
 
+  def destroy
+    delete_order = UserAnimation.where(id: params[:id])[0]
+    if delete_order.destroy
+      flash[:alert] = "Order Deleted Successfully"
+      redirect_to user_animations_path
+    else
+      flash[:alert] = "Couldn't delete"
+      redirect_to user_animations_path
+    end
+  end
+
+  def edit
+    @edit_order = UserAnimation.where(id: params[:id])[0]
+  end
+
+  def update
+    update_user = UserAnimation.where(id: params[:id])[0]
+    if edit_validate_date
+      if update_user.update(permit_params)
+        flash[:notice] = "Order Updated Successfully"
+        redirect_to user_animations_path
+      else
+        redirect_to user_animation_path
+      end
+    else
+      flash[:alert] = "Couldn't Update Order !!!"
+      redirect_to edit_user_animation_path
+    end
+  end
+
+  def inactive
+    inactive_order = UserAnimation.where(id: params[:id])[0]
+    inactive_order.update(status: "Inactive")
+    flash[:notice] = "Deactivated Successfully"
+    redirect_to user_animations_path
+  end
   private
 
     def permit_params
@@ -37,16 +72,30 @@ class UserAnimationsController < ApplicationController
     def validate_date
       new_start = params[:user_animation][:start_date]
       new_end = params[:user_animation][:end_date]
-      @users = UserAnimation.where(user_id: current_user.id, location: params[:user_animation][:location])
-      (@users).none? { |user| (new_start.to_date >= user.start_date && new_start.to_date <= user.end_date) ||
-      (new_start.to_date <= user.start_date && new_end.to_date >= user.start_date) } ? true : false
+      location = params[:user_animation][:location]
+      @users = UserAnimation.where(user_id: current_user.id, location: location, status: "Active")
+      @users.none? do |user|
+        (new_start.to_date >= user.start_date && new_start.to_date <= user.end_date) ||
+        (new_start.to_date <= user.start_date && new_end.to_date >= user.start_date)
+      end
+    end
+
+    def edit_validate_date
+      new_start = params[:user_animation][:start_date]
+      new_end = params[:user_animation][:end_date]
+      location = params[:user_animation][:location]
+      @orders = UserAnimation.where(user_id: current_user.id, location: location, status: "Active")
+                .where.not(id: params[:id])
+      @orders.none? do |order|
+        (new_start.to_date >= order.start_date && new_start.to_date <= order.end_date) ||
+        (new_start.to_date <= order.start_date && new_end.to_date >= order.start_date)
+      end
     end
 
     def redirect_to_new
       flash[:alert] = "Animation already added for #{params[:user_animation][:location]}"
       redirect_to purchase_path(params[:user_animation][:category_id],
-                                params[:user_animation][:sub_category_id],
-                                params[:user_animation][:animation_data_id])
+        params[:user_animation][:sub_category_id], params[:user_animation][:animation_data_id])
     end
 
 end
