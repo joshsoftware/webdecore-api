5181263d77ad968d64a3172a1b67850505f3dbd3
PreetiDantkale <dantkalepreeti29@gmail.com>
2020-02-23 12:23:42 +0530

    Added frequency to user animation file

--- db/schema.rb
+++ db/schema.rb
@@ -71,6 +71,4 @@ ActiveRecord::Schema.define(version: 2020_02_20_095942) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
-  add_foreign_key "themes", "users"
 end
